0e355574659cce5519a0ff33ba9067d7d037fe54
nithish1998 <nithish09091998@gmail.com>
2023-10-13 17:42:29 +0530

    intial commit

---
+++ src/main/java/com/oasys/webapp/Controller.java
@@ -0,0 +1,14 @@
+package com.oasys.webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+		@GetMapping(value="/get")
+		public String get() {
+			return "Success";
+		}
+
+}
